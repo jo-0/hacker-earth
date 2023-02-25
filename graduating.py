73d5bdc2f8e16ecfa83d5bad436061e1fb590542
Ajo <17722952+jo-0@users.noreply.github.com>
2023-02-25 21:02:02 +0530

    find all possible ways to attend class

--- graduating.py
+++ graduating.py
@@ -1,3 +1,15 @@
 # https://www.hackerearth.com/problem/algorithm/graduating-5b72b1c7-bc102a51/
 
 
+from itertools import product
+
+
+def find_all_possible_attendance_combinations(n) -> list[str]:
+    """
+    Lets call the day she's absent as A. And the day she's present as P, find all possible ways to attend classes for
+    'n' days.
+    """
+    return ["".join(attendance) for attendance in product(["A", "P"], repeat=n)]
+
+
+find_all_possible_attendance_combinations(5)
