4f477d575826e9f201525ac797d8ab5a63895a17
Ajo <17722952+jo-0@users.noreply.github.com>
2023-02-26 17:59:15 +0530

    rm readme, and update comments

--- graduating.py
+++ graduating.py
@@ -1,10 +1,26 @@
+"""
 # https://www.hackerearth.com/problem/algorithm/graduating-5b72b1c7-bc102a51/
 
+Explanation with example:
+
+(if you are not allowed to miss classes for two or more consecutive days)
+
+Let's call the day she's absent as A. And the day she's present as P
+
+Then all possible attendance combinations for n=3 days will be -
+AAA, AAP, APA, APP, PAA, PAP, PPA, and PPP -> Now in combinations AAA, AAP, and PAA, she's absent for more than or equal to two consecutive days. Thus, we'll reject these.
+
+Required Combinations: APA, APP, PAP, PPA, and PPP -> 5 ways. Print 5
+
+Now, in 2 combinations (APA & PPA) from 5 possible combinations, she'll not be able to attend the ceremony.
+
+Thus, print Probability as 2/5
+"""
 
 from itertools import product
 
 
-def find_possible_attendance_combinations(n) -> list[str]:
+def find_possible_ways_to_attend(n) -> list[str]:
     """
     Lets call the day she's absent as A. And the day she's present as P, find all possible ways to attend classes for
     'n' days.
@@ -18,11 +34,11 @@ def find_possible_attendance_combinations(n) -> list[str]:
 
 
 def find_probability_of_missing(ways_to_attend) -> list[str]:
-    # The probability that the student will miss the graduation ceremony is high if she's absent on the last day.
+    # The student will miss the graduation ceremony if she's absent on the last day.
     chance_of_missing = [way for way in ways_to_attend if way[-1] == "A"]
     return chance_of_missing
 
 
-possible_ways = find_possible_attendance_combinations(n=5)
+possible_ways = find_possible_ways_to_attend(n=5)
 probability = find_probability_of_missing(ways_to_attend=possible_ways)
 print(f"{len(probability)}/{len(possible_ways)}")
